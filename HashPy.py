7c74173dcc0af04135f9db76c7d6593fccc84716
elinkamandarinkazag <129305908+notelina@users.noreply.github.com>
2025-10-02 13:56:14 +0300

    HashPy.py

---
+++ HashPy.py
@@ -0,0 +1,9 @@
+def basic_example():
+    student_grades = {}
+    student_grades["Alice"] = 95
+    student_grades["Bob"] = 87
+    
+    print("Alice:", student_grades["Alice"])
+    print("Bob:", student_grades["Bob"])
+
+basic_example()
